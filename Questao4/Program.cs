06c868fd2b8db725461c99d3de72d15475ae9006
George Wanderson <54476900+GeorgeWanderson@users.noreply.github.com>
2022-04-01 17:38:40 -0300

    uploaded other questions

---
+++ Questao4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
